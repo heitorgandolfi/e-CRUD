c71c218715cab7d8b245835966b5eedb4c410b5a
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2023-01-23 17:52:32 -0300

    update JS
    Finalizada a criação do CRUD em si. Adicionada verificação dos inputs.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 const newClient = document.querySelector(".new__client");
 const modal = document.querySelector(".modal");
-const cancelBtn = document.querySelector(".cancel");
 
+const form = document.querySelector(".form");
+const saveBtn = document.querySelector(".save__client");
+const closeBtn = document.querySelector(".cancel");
+const fields = document.querySelectorAll(".modal__field")
+const clientName = document.querySelector(".name");
+const clientCpf = document.querySelector(".cpf");
+const clientTel = document.querySelector(".tel");
+const clientEmail = document.querySelector(".email");
+const clientCity = document.querySelector(".city");
+const clientValue = document.querySelector(".value");
+const clientDate = document.querySelector(".date");
 
+// Funções
 
 const tempClients = {
-    name: "Heitor",
+    name: "Juve",
     CPF: "46729111879",
     telephone: "16991290513",
     email: "heitor@gmail.com",
@@ -14,18 +25,68 @@ const tempClients = {
     date: "15/12/12"
 }
 
+// CRUD
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem("client")) ?? [];
+const setLocalStorage = (listClients) => localStorage.setItem("client", JSON.stringify(listClients));
+
+const deleteClient = (index) => {
+    const listClients = readClient();
+    listClients.splice(index, 1);
+    setLocalStorage(listClients)
+}
+
+const updateClient = (index, client) => {
+    const listClients = readClient()
+    listClients[index] = client
+    setLocalStorage(listClients)
+}
+
+const readClient = () => getLocalStorage();
+
 const createClient = (client) => {
-    const listClients = JSON.parse(localStorage.getItem("client"));
-    listClients.push(client);
-    localStorage.setItem("client", JSON.stringify(listClients));
+    const listClients = getLocalStorage()
+    listClients.push(client)
+    setLocalStorage(listClients)
+}
+
+const isValidFields = () => {
+    return form.reportValidity()
+}
+
+// Layout Interact
+
+const closeModal = () => {
+    clearFields();
+    modal.style.display = "none"
+}
+
+clearFields = () => {
+    fields.forEach((field) => field.value = "")
+}
+
+const saveClient = () => {
+    if (isValidFields()) {
+        const client = {
+            name: clientName.value,
+            CPF: clientCpf.value,
+            telephone: clientTel.value,
+            email: clientEmail.value,
+            city: clientCity.value,
+            value: clientValue.value,
+            date: clientDate.value
+        }
+        createClient(client);
+        closeModal();
+    }
 }
 
 // Eventos
 
 newClient.addEventListener("click", () => {
     modal.style.display = "block"
-})
+});
 
-cancelBtn.addEventListener("click", () => {
-    modal.style.display = "none"
-})
+saveBtn.addEventListener("click", saveClient)
+
+closeBtn.addEventListener("click", closeModal);
