dcfac5cd49279633c838203ae158dbfad981871f
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2023-01-29 10:41:47 -0300

    update jS
    Adicionado o método Intl, a fim de padronizar o retorno dos valores inseridos pelos usuários.

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const tableBtn = document.querySelector("#clients__table>tbody");
 const form = document.querySelector(".form");
 const saveBtn = document.querySelector(".save__client");
 const closeBtn = document.querySelector(".cancel");
-const fields = document.querySelectorAll(".modal__field")
+const fields = document.querySelectorAll(".modal__field");
 const clientName = document.querySelector(".name");
 const clientCpf = document.querySelector(".cpf");
 const clientTel = document.querySelector(".tel");
@@ -21,143 +21,151 @@ const clientDate = document.querySelector(".date");
 
 // CRUD
 
-const setLocalStorage = (listClients) => localStorage.setItem("client", JSON.stringify(listClients));
+const setLocalStorage = (listClients) =>
+  localStorage.setItem("client", JSON.stringify(listClients));
 const getLocalStorage = () => JSON.parse(localStorage.getItem("client")) ?? [];
 
 const deleteClient = (index) => {
-    const listClients = readClient();
-    listClients.splice(index, 1);
-    setLocalStorage(listClients)
-}
+  const listClients = readClient();
+  listClients.splice(index, 1);
+  setLocalStorage(listClients);
+};
 
 const updateClient = (index, client) => {
-    const listClients = readClient()
-    listClients[index] = client
-    setLocalStorage(listClients)
-}
+  const listClients = readClient();
+  listClients[index] = client;
+  setLocalStorage(listClients);
+};
 
 const readClient = () => getLocalStorage();
 
 const createClient = (client) => {
-    const listClients = getLocalStorage()
-    listClients.push(client)
-    setLocalStorage(listClients)
-}
+  const listClients = getLocalStorage();
+  listClients.push(client);
+  setLocalStorage(listClients);
+};
 
 const isValidFields = () => {
-    return form.reportValidity()
-}
+  return form.reportValidity();
+};
 
 // Layout Interact
 
 const openModal = () => {
-    modal.style.display = "block";
-    article.classList.toggle("active");
-    header.classList.toggle("active");
-}
+  modal.style.display = "block";
+  article.classList.toggle("active");
+  header.classList.toggle("active");
+};
 
 const closeModal = () => {
-    clearFields();
-    modal.style.display = "none";
-    article.classList.toggle("active");
-    header.classList.toggle("active");
-}
+  clearFields();
+  modal.style.display = "none";
+  article.classList.toggle("active");
+  header.classList.toggle("active");
+};
 
 clearFields = () => {
-    fields.forEach((field) => field.value = "")
-}
+  fields.forEach((field) => (field.value = ""));
+};
 
 const saveClient = () => {
-    if (isValidFields()) {
-        // let enUsDate = clientDate.value;
-        // let ptBrDate = enUsDate.split("-").reverse().join("/");
-        const client = {
-            name: clientName.value,
-            cpf: clientCpf.value,
-            telephone: clientTel.value,
-            email: clientEmail.value,
-            city: clientCity.value,
-            value: clientValue.value,
-            date: clientDate.value
-        }
-        const index = document.querySelector(".name").dataset.index;
-        if (index == "new") {
-            createClient(client);
-            updateTable();
-            closeModal();
-        } else {
-            updateClient(index, client);
-            updateTable();
-            closeModal();
-        }
+  if (isValidFields()) {
+    const formatValue = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(clientValue.value);
+    // let enUsDate = clientDate.value;
+    // let ptBrDate = enUsDate.split("-").reverse().join("/");
+    const client = {
+      name: clientName.value,
+      cpf: clientCpf.value,
+      telephone: clientTel.value,
+      email: clientEmail.value,
+      city: clientCity.value,
+      value: formatValue,
+      date: clientDate.value,
+    };
+    const index = document.querySelector(".name").dataset.index;
+    if (index == "new") {
+      createClient(client);
+      updateTable();
+      closeModal();
+    } else {
+      updateClient(index, client);
+      updateTable();
+      closeModal();
     }
-}
+  }
+};
 
 const createRow = (client, index) => {
-    const newRow = document.createElement("tr");
-    newRow.innerHTML = `
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
     <td>${client.name}</td>
     <td>${client.cpf}</td>
     <td>${client.telephone}</td>
     <td>${client.email}</td>
     <td>${client.city}</td>
-    <td>R$ ${client.value}</td>
+    <td>${client.value}</td>
     <td>${client.date}</td>
     <td>
         <button class="edit__client" type="button" data-action="edit-${index}">Editar</button> |
         <button class="delete__client" type="button" data-action="delete-${index}">Excluir</button>
     </td>
- `
-    const tableClient = document.querySelector("#clients__table>tbody");
-    tableClient.appendChild(newRow);
-}
+ `;
+  const tableClient = document.querySelector("#clients__table>tbody");
+  tableClient.appendChild(newRow);
+};
 
 const clearTable = () => {
-    const rows = document.querySelectorAll("#clients__table>tbody tr");
-    rows.forEach(row => row.parentNode.removeChild(row));
-}
+  const rows = document.querySelectorAll("#clients__table>tbody tr");
+  rows.forEach((row) => row.parentNode.removeChild(row));
+};
 
 const updateTable = () => {
-    const listClients = readClient();
-    clearTable();
-    listClients.forEach(createRow);
-}
+  const listClients = readClient();
+  clearTable();
+  listClients.forEach(createRow);
+};
 
 const fillFields = (client) => {
-    clientName.value = client.name
-    clientCpf.value = client.cpf
-    clientTel.value = client.telephone
-    clientEmail.value = client.email
-    clientCity.value = client.city
-    clientValue.value = client.value
-    clientDate.value = client.date
-    clientName.dataset.index = client.index
-}
+  clientName.value = client.name;
+  clientCpf.value = client.cpf;
+  clientTel.value = client.telephone;
+  clientEmail.value = client.email;
+  clientCity.value = client.city;
+  clientValue.value = client.value;
+  clientDate.value = client.date;
+  clientName.dataset.index = client.index;
+};
 
 const editClient = (index) => {
-    const client = readClient()[index];
-    client.index = index;
-    fillFields(client);
-    openModal()
-}
+  const client = readClient()[index];
+  client.index = index;
+  fillFields(client);
+  openModal();
+};
 
 const editDelete = (e) => {
-    if (e.target.type == "button") {
-        const [action, index] = e.target.dataset.action.split("-")
+  if (e.target.type == "button") {
+    const [action, index] = e.target.dataset.action.split("-");
 
-        if (action == "edit") {
-            editClient(index)
-        } else {
-            const client = readClient()[index]
-            const response = confirm(`Deseja realmente excluir os dados de ${client.name}?`)
-            if (response) {
-                deleteClient(index)
-                updateTable()
-            }
-        }
-
+    if (action == "edit") {
+      editClient(index);
+    } else {
+      const client = readClient()[index];
+      const response = confirm(
+        `Deseja realmente excluir os dados de ${client.name}?`
+      );
+      if (response) {
+        deleteClient(index);
+        updateTable();
+      }
     }
-}
+  }
+};
 
 updateTable();
 
